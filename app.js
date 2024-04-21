9d873d740adca09b8747fd94c3fc94be40a1b23a
吕佩宗 <lvpeizong@lvpeizongdeMac-mini.local>
2024-04-21 10:57:10 +1000

    add tour controller

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 const express = require("express");
+const mongoose = require("mongoose");
 const app = express();
+const tourController = require("./controllers/tourController");
 
 const port = 8000;
 app.get("/", (req, res) => {
@@ -9,6 +11,20 @@ app.get("/", (req, res) => {
     count: 5,
   });
 });
+
+app.route("/api/v1/tours").get(tourController.getAllTours);
+
+const DB =
+  "mongodb+srv://felikslyu:dXLmmahiSUFuAkgD@cluster0.kninbn4.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+  })
+  .then(() => console.log("DB connection successful!"))
+  .catch((err) => {
+    console.error("DB connection failed:", err);
+  });
+
 app.listen(port, () => {
   console.log(`listen to ${port}`);
 });
